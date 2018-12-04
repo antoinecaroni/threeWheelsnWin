2b7d4d6f9c9b4ebcd3a4719d494c76a2a50c6cde
antoinecaroni <antoine.caroni@hotmail.fr>
2018-12-04 18:55:42 +0100

    start form for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,19 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    authorize @booking
   end
 
   def create
+    @booking = Booking.new
+    authorize @booking
   end
 
   def delete
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+    authorize @booking
   end
 end
