5e6dc140c54221813bdb6cfee8ee56ae6c9e6a53
antoinecaroni <antoine.caroni@hotmail.fr>
2018-12-05 12:46:20 +0100

    booking new coded without validates dates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :vehicle
   has_one :review
   validates :start_date, presence: true
-  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :end_date, presence: true
   validates :profile_id, presence: true
   validates :vehicle_id, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
